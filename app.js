382ed0fc989b158ad89c8b592826b0cfc8e68f54
jaylin94 <jaylin94@gmail.com>
2019-07-07 09:54:10 -0400

    Edit server config for Heroku

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
   res.render("colorGame");
 });
 
-app.listen(9392, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
